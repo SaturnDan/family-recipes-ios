3e884154498bb7b5d1ad60bd178f6a0202e30916
Dan <saturndan@protonmail.com>
2023-01-15 00:11:02 +1100

    Initial Commit

---
+++ family-recipes-ios/family_recipes_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  family_recipes_iosApp.swift
+//  family-recipes-ios
+//
+//  Created by Dan Malyaris on 15/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct family_recipes_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
